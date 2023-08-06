439fbaa84d94c9a3ba211ce67babe648c71cb628
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2023-08-06 09:15:00 +0530

    Recyclable and Low Fat Products pd commit

---
+++ leetcode_pandas/2RecyclableandLowFatProducts.py
@@ -0,0 +1,13 @@
+# 1757. Recyclable and Low Fat Products
+# product_id is the primary key (column with unique values) for this table.
+# low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+# recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+# Write a solution to find the ids of products that are both low fat and recyclable.
+# Return the result table in any order.
+# The result format is in the following example.
+
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    df = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
+    return df[['product_id']]
