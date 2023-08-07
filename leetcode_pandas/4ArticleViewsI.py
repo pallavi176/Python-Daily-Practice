3f8aa0b74538b60d847507b80731e94f365527b9
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2023-08-07 07:47:02 +0530

    updated

--- leetcode_pandas/4ArticleViewsI.py
+++ leetcode_pandas/4ArticleViewsI.py
@@ -43,3 +43,22 @@ def article_views(views: pd.DataFrame) -> pd.DataFrame:
     df = df[['author_id']].rename(columns={'author_id': 'id'})
     df_unique = df.drop_duplicates(subset='id', keep='first')
     return df_unique.sort_values(by='id', ascending=True)
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    return views[views['author_id'] == views['viewer_id']][['author_id']].drop_duplicates().rename(columns={'author_id': 'id'}).sort_values(by='id', ascending=True)
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    # Filter rows where author_id and viewer_id are the same (authors viewing their own articles)
+    authors_viewed_own_articles = views[views['author_id'] == views['viewer_id']]
+    
+    # Get unique author_ids from the filtered data
+    unique_authors = authors_viewed_own_articles['author_id'].unique()
+    
+    # Sort the unique author_ids in ascending order
+    unique_authors = sorted(unique_authors)
+    
+    # Create a DataFrame with the sorted unique author_ids and rename the 'author_id' column to 'id'
+    result_df = pd.DataFrame({'id': unique_authors})
+    
+    return result_df
+
